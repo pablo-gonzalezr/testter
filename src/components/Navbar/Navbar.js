80b1344bf0e46850a9c00fef8bffab8a4324c3a6
Rementeria <p.gonzalez@outlook.com>
2021-02-06 17:26:29 -0300

    fix

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -9,9 +9,6 @@ const Navbar = () => {
         <Link to="/" className="navbar-brand mr-5">
           Buscador de Peliculas
         </Link>
-        <a class="navbar-brand" href="/testter">
-          Home
-        </a>
         <Link to="/favoritos" className="btn btn-outline-info ml-3">
           Favoritos
         </Link>
